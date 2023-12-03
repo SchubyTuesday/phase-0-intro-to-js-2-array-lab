cc6c0bbae30766da846ee7aaff2dd4537849112c
Charlie Schubach <charlie.schubach@gmail.com>
2023-12-02 22:11:46 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name) {
+    cats = cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats = [name, ...cats]
+}
